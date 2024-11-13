ebd1497778855580a08c7b80485270a99813952f
Aysel Huseynova <aysellhuseeyn@gmail.com>
2024-11-13 12:59:30 +0400

    Changed

--- src/lesson8/Main.java
+++ src/lesson8/Main.java
@@ -1,15 +1,7 @@
-package lesson8;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Author fd=new Author("Fyodor", "Dostoevsky", 1821);
-        Book bk=new Book("Crime and Punishment", 1866, fd);
-
-        System.out.println(fd);
-
-        int a=5;
-        Integer b=5;
-        System.out.println(b.doubleValue());
 
     }
 }
